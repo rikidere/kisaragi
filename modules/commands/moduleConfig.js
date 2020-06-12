bae4fb929df2aba14af76de23d3d21fa1bfdd9d0
rikidere <rorikidesu@gmail.com>
2020-06-12 22:51:12 +0200

    disabled osc group for now

--- modules/commands/moduleConfig.js
+++ modules/commands/moduleConfig.js
@@ -4,9 +4,11 @@ module.exports = {
 		{
 			path: 'util', name: 'util'
 		},
+		/*
 		{
 			path: 'osc', name: 'osu! swiss community'
 		},
+		*/
 		{
 			path: 'commands', name: 'general commands'
 		}
